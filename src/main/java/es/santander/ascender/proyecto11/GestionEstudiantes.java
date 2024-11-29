f37369c5de3484c8767b62434cde6999530721fe
grupo01 <notengocorreo@notengo.com>
2024-11-29 13:41:25 +0100

    Tarea casa hacer

---
+++ src/main/java/es/santander/ascender/proyecto11/GestionEstudiantes.java
@@ -0,0 +1,44 @@
+package es.santander.ascender.proyecto11;
+
+import java.util.Map;
+import java.util.Set;
+
+public class GestionEstudiantes implements IGestionEstudiantes{
+
+    @Override
+    public boolean agregarEstudiante(String nombre, int calificacion) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'agregarEstudiante'");
+    }
+
+    @Override
+    public Integer obtenerCalificacion(String nombre) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerCalificacion'");
+    }
+
+    @Override
+    public Map<String, Integer> obtenerEstudiantesYCalificaciones() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerEstudiantesYCalificaciones'");
+    }
+
+    @Override
+    public boolean existeEstudiante(String nombre) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'existeEstudiante'");
+    }
+
+    @Override
+    public boolean eliminarEstudiante(String nombre) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEstudiante'");
+    }
+
+    @Override
+    public void agregarEstudiantes(Set<String> nuevosEstudiantes, Map<String, Integer> nuevasCalificaciones) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'agregarEstudiantes'");
+    }
+
+}
